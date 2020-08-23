a3a099c334f4300b323412266771d6db2686baa1
Seema Neloufer <40924685+seemaneloufer@users.noreply.github.com>
2020-08-24 03:41:39 +0400

    old footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,191 +1,156 @@
-<!--footer start-->
-<footer class="footer widget-footer clearfix">
-    <div class="first-footer ttm-bgcolor-skincolor ttm-bg ttm-bgimage-yes bg-img1">
-        <div class="ttm-row-wrapper-bg-layer ttm-bg-layer"></div>
-        <div class="container">
-            <div class="row align-items-md-center">
-                <div class="col-lg-4 col-md-4 col-sm-12 order-md-2">
-                    <div class="footer-logo text-sm-center">
-                        <img src="images/footer-logo.png" alt="footer-logo">
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-6 order-md-1">
-                    <div class="text-left">
-                        <!--  featured-icon-box -->
-                        <div class="featured-icon-box left-icon icon-align-top">
-                            <div class="featured-icon"><!--  featured-icon -->
-                                <div class="ttm-icon ttm-icon_element-color-white ttm-icon_element-size-md">
-                                    <i class="ti ti-location-pin"></i><!--  ttm-icon -->
-                                </div>
-                            </div>
-                            <div class="featured-content"><!--  featured-content -->
-                                <div class="featured-desc">
-                                    <p>4789 Melmorn Street, Zakilahon Ton Mashintron Town USA</p>
-                                </div>
-                            </div>
-                        </div><!--  featured-icon-box END -->
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-6 order-md-3">
-                    <div class="text-sm-right">
-                        <a class="ttm-btn ttm-btn-size-md ttm-btn-style-border ttm-icon-btn-left ttm-btn-color-white" href="mailto:Info@example.com" title=""> Info@example.com <i class="fa fa-envelope-o"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color pt-4">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Footer links -->
+    <div class="row text-center text-md-left mt-3 pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">BridgingFX</h6>
+        <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+          consectetur
+          adipisicing elit.</p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Trading Services</h6>
+        <p>
+          <a class="ft-txt" href="#!">Spot Currencies</a>
+        </p>
+        <p>
+          <a  class="ft-txt" href="#!">Spot Indices</a>
+        </p>
+        <p>
+          <a class="ft-txt"  href="#!">Metals</a>
+        </p>
+        <p>
+          <a class="ft-txt"  href="#!">Commodity CFD</a>
+        </p>
+        <p>
+          <a class="ft-txt"  href="#!">Cryptocurrencies</a>
+        </p>
+      </div>
+      <!-- Grid column -->
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 style="white-space: nowrap;" class="text-uppercase mb-4 font-weight-bold">Trading Platform</h6>
+        <p>
+          <a  class="ft-txt" href="#!">Overview MT5 Platform</a>
+        </p>
+        <p>
+          <a class="ft-txt"  href="#!">MT5 For Window</a>
+        </p>
+        <p>
+          <a class="ft-txt"  href="#!">MT5 For Android</a>
+        </p>
+        <p>
+          <a  class="ft-txt" href="#!">MT5 For IOS</a>
+        </p>
+        <p>
+          <a class="ft-txt"  href="#!">Trading Account</a>
+        </p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 style="white-space: nowrap;" class="text-uppercase mb-4 font-weight-bold">Trading Tools</h6>
+        <p>
+          <a class="ft-txt"  href="#!">PAMM Account</a>
+        </p>
+        <p>
+          <a  class="ft-txt" href="#!">PAMM FAQ</a>
+        </p>
+        <p>
+          <a class="ft-txt"  href="#!">MQL5 Trading Signals</a>
+        </p>
+        <p>
+          <a class="ft-txt"  href="#!">VPS Services</a>
+        </p>
+        <p>
+          <a class="ft-txt"  href="#!">Robot Indicators</a>
+        </p>
+      </div>
+
+      <!-- Grid column -->
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Get In Touch</h6>
+        <p>
+          <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+        <p>
+          <i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+        <p>
+          <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+      </div>
+      <!-- Grid column -->
+
     </div>
-    <div class="second-footer ttm-textcolor-white bg-img2">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 widget-area">
-                    <div class="widget widget_text  clearfix">
-                        <h3 class="widget-title">About Our Company</h3>
-                        <div class="textwidget widget-text">
-                            We are the best world Information Technology Company. Providing the highest quality in hardware &amp; Network solutions. About more than 20 years of experience and 1000 of innovative achievements.
-                        </div>
-                        <div class="quicklink-box">
-                            <!--  featured-icon-box -->
-                            <div class="featured-icon-box left-icon">
-                                <div class="featured-icon"><!--  featured-icon -->
-                                    <div class="ttm-icon ttm-icon_element-style-round ttm-icon_element-bgcolor-skincolor ttm-icon_element-size-md ttm-icon_element-style-round">
-                                        <span class="flaticon flaticon-clock"></span><!--  ttm-icon -->
-                                    </div>
-                                </div>
-                                <div class="featured-content"><!--  featured-content -->
-                                    <div class="featured-desc"><!--  featured-desc -->
-                                        <p>Talk To Our Support</p>
-                                    </div>
-                                    <div class="featured-title"><!--  featured-title -->
-                                        <h5>+1 002-123-4567</h5>
-                                    </div>
-                                </div>
-                            </div><!--  featured-icon-box END -->
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 widget-area">
-                    <div class="widget link-widget clearfix">
-                       <h3 class="widget-title">Quick Links</h3>
-                        <ul id="menu-footer-services">
-                            <li><a href="#">Make Appointment</a></li>
-                            <li><a href="#">Customer Services</a></li>
-                            <li><a href="#">Department Services</a></li>
-                            <li><a href="#">About Company</a></li>
-                            <li><a href="#">Our Case Studies</a></li>
-                            <li><a href="#">Free Consultation</a></li>
-                            <li><a href="#">Meet Our Experts</a></li>
-                            <li><a href="#">Our Business Growth</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 widget-area">
-                    <div class="widget widget_text clearfix">
-                        <h3 class="widget-title">Latest News</h3>
-                        <ul class="widget-post ttm-recent-post-list">
-                            <li>
-                                <a href="single-blog.html"><img src="images/blog/01.jpg" alt="post-img"></a>
-                                <a href="single-blog.html">Define World Best IT Solution Technology</a>
-                                <span class="post-date"><i class="fa fa-calendar"></i>May 01, 2019</span>
-                            </li>
-                            <li>
-                                <a href="single-blog.html"><img src="images/blog/02.jpg" alt="post-img"></a>
-                                <a href="single-blog.html">You Must Try 20 Secret Of Digital Transform</a>
-                                <span class="post-date"><i class="fa fa-calendar"></i>May 03, 2019</span>
-                            </li>
-                            <li>
-                                <a href="single-blog.html"><img src="images/blog/03.jpg" alt="post-img"></a>
-                                <a href="single-blog.html">10 PHP Frameworks You Need To Use Anywhere</a>
-                                <span class="post-date"><i class="fa fa-calendar"></i>May 05, 2019</span>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 widget-area">
-                    <div class="widget flicker_widget clearfix">
-                       <h3 class="widget-title">News Letter</h3>
-                       <div class="textwidget widget-text">
-                            Sign up today for hints, tips and the latest product news
-                            <form id="subscribe-form" class="newsletter-form" method="post" action="#" data-mailchimp="true">
-                                <div class="mailchimp-inputbox clearfix" id="subscribe-content">
-                                    <i class="fa fa-envelope"></i>
-                                    <input type="email" name="email" placeholder="Email Address.." required="">
-                                    <input type="submit" value="">
-                                </div>
-                                <div id="subscribe-msg"></div>
-                            </form>
-                            <h5 class="mb-20">Follow Us On</h5>
-                            <div class="social-icons circle social-hover">
-                                <ul class="list-inline">
-                                    <li class="social-facebook"><a class="tooltip-top" target="_blank" href="#" data-tooltip="Facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                    <li class="social-twitter"><a class="tooltip-top" target="_blank" href="#" data-tooltip="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                    <li class="social-flickr"><a class=" tooltip-top" target="_blank" href="#" data-tooltip="flickr"><i class="fa fa-flickr" aria-hidden="true"></i></a></li>
-                                    <li class="social-linkedin"><a class=" tooltip-top" target="_blank" href="#" data-tooltip="LinkedIn"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <!-- Footer links -->
+
+    <hr>
+
+    <!-- Grid row -->
+    <div class="row d-flex align-items-center">
+
+      <!-- Grid column -->
+      <div class="col-md-7 col-lg-8">
+
+
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-5 col-lg-4 ml-lg-0">
+
+        <!-- Social buttons -->
+        <div class="text-center text-md-right">
+          <ul class="list-unstyled list-inline">
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-google-plus-g"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
         </div>
+
+      </div>
+      <!-- Grid column -->
+
     </div>
-    <div class="bottom-footer-text ttm-bgcolor-darkgrey ttm-textcolor-white">
-        <div class="container">
-            <div class="row copyright">
-                <div class="col-md-6">
-                    <div class="">
-                        <span>Copyright © 2019&nbsp;<a href="#">Altech</a> Template by <a href="https://themetechmount.com/" target="_blank">Themetechmount</a></span>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="text-md-right res-767-mt-10">
-                        <div class="d-lg-inline-flex">
-                            <ul id="menu-footer-menu" class="footer-nav-menu">
-                                <li><a href="#">About Us</a></li>
-                                <li><a href="#">Services</a></li>
-                                <li><a href="#">Privacy</a></li>
-                            </ul>
-                            <div class="float-md-right ml-20 res-767-ml-0">
-                                <img src="images/footer-payment-img.png" alt="payment-img">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
 </footer>
-<!--footer end-->
-
-<!--back-to-top start-->
-<a id="totop" href="#top">
-    <i class="fa fa-angle-up"></i>
-</a>
-<!--back-to-top end-->
-
-</div><!-- page end -->
-
-<!-- Javascript -->
-
-<script src="js/jquery.min.js"></script>
-<script src="js/tether.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.easing.js"></script>
-<script src="js/jquery-waypoints.js"></script>
-<script src="js/jquery-validate.js"></script>
-<script src="js/owl.carousel.js"></script>
-<script src="js/jquery.prettyPhoto.js"></script>
-<script src="js/numinate.min6959.js?ver=4.9.3"></script>
-<script src="js/lazysizes.min.js"></script>
-<script src="js/main.js"></script>
-
-<!-- Revolution Slider -->
-<script src="revolution/js/revolution.tools.min.js"></script>
-<script src="revolution/js/rs6.min.js"></script>
-<script src="revolution/js/slider.js"></script>
-
-<!-- Javascript end-->
-
-</body>
-
-<!-- Mirrored from themetechmount.com/html/altech/home-3.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 21 Aug 2020 23:45:19 GMT -->
-</html>
+<!-- Footer -->
